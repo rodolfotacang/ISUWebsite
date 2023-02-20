0f7fe1a34ddb9af3002db204789e98e085c61e58
rodolfotacang <rodolfotacang123@gmail.com>
2023-02-20 09:53:30 +0800

    added space for sample

--- Index.php
+++ Index.php
@@ -13,6 +13,8 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   
+
+        
         <style>
             .header-image{
                 background: linear-gradient(to bottom, rgb(0, 96, 47) 0%, rgb(1, 177, 52) 88%, rgb(1, 192, 52) 100%);
