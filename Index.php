476c5f77bea890e83138b523955cae6842d20011
rodolfotacang <rodolfotacang123@gmail.com>
2023-02-20 09:57:50 +0800

    demmet

--- Index.php
+++ Index.php
@@ -14,7 +14,7 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   
 
-        
+
         <style>
             .header-image{
                 background: linear-gradient(to bottom, rgb(0, 96, 47) 0%, rgb(1, 177, 52) 88%, rgb(1, 192, 52) 100%);
@@ -60,7 +60,7 @@
 
 </div>
 </section>
-
+<!-- comment -->
         <div class="d-flex " id="wrapper">
             <!-- Sidebar-->
             <div class="border-end bg-white " id="sidebar-wrapper" >
